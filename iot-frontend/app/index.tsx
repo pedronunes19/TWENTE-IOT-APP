ffb274e488536fd525da15a58a29dca9e0e83c35
pedronunes19 <pedromiguelmnunes@gmail.com>
2024-11-01 23:19:46 +0000

    app

---
+++ iot-frontend/app/index.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text className="text-indigo-600">things in the internet</Text>
+    </View>
+  );
+}
