0e0373d1a1355d29f3cf1d784a824444f14ea253
pedronunes19 <pedromiguelmnunes@gmail.com>
2024-11-08 18:21:07 +0100

    :boom:

--- iot-frontend/app/index.tsx
+++ iot-frontend/app/index.tsx
@@ -1,151 +1,79 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, Image, Button, PermissionsAndroid, Platform } from 'react-native';
-
-let BleManager;
-/*if (Platform.OS !== 'web') {
-  // Only import `react-native-ble-plx` on mobile
-  BleManager = require('react-native-ble-plx').BleManager;
-}*/
+import { Text, View, Image } from 'react-native';
 
 // Importing images
 import stillImage from '../assets/images/still.png';
 import runningImage from '../assets/images/running.png';
 import pushUpsImage from '../assets/images/push-ups.png';
 import squatsImage from '../assets/images/squats.png';
-// import jumpingJacksImage from '../assets/images/jumping-jacks.png';
-
-const DEVICE_ADDRESS = "C1:B0:66:6D:31:B7";  // Replace with the address of your device
 
 export default function App() {
-  const activityNames = ["Still", "Running", "Push-Ups", "Squats", "Jumping Jacks"];
+  const activityNames = ["Still", "Running", "Push-Ups", "Squats"];
   
   const activities = new Map([
     ["Still", stillImage],
     ["Running", runningImage],
     ["Push-Ups", pushUpsImage],
-    ["Squats", squatsImage],
-    ["Jumping Jacks", stillImage]
+    ["Squats", squatsImage]
   ]);
 
-  const [currentActivity, setCurrentActivity] = useState(activityNames[2]);
-  const [currentActivityPNG, setCurrentActivityPNG] = useState(activities.get("Push-Ups"));
-  const [deviceData, setDeviceData] = useState(null);
+  const [currentActivityIndex, setCurrentActivityIndex] = useState(0);
+  const [timeElapsed, setTimeElapsed] = useState(0);
 
-  const manager = Platform.OS !== 'web' ? new BleManager() : null;
+  const currentActivity = activityNames[currentActivityIndex];
+  const currentActivityPNG = activities.get(currentActivity);
 
-  async function connectWithRetry(device, maxRetries = 3, timeout = 20000) {
-    let attempt = 0;
-    let server = null;
-  
-    while (attempt < maxRetries) {
-      try {
-        console.log(`Attempt ${attempt + 1} to connect to device...`);
-        
-        const connectionPromise = device.gatt.connect();
-        const timeoutPromise = new Promise((_, reject) =>
-          setTimeout(() => reject(new Error("Connection timeout")), timeout)
-        );
-  
-        // Race between connection attempt and timeout
-        server = await Promise.race([connectionPromise, timeoutPromise]);
-  
-        if (server && server.connected) {
-          console.log("Connected to GATT server");
-          return server; // Successfully connected
-        }
-      } catch (error) {
-        console.error(`Attempt ${attempt + 1} failed:`, error);
-      }
-  
-      attempt++;
-      console.log(`Retrying (${attempt}/${maxRetries})...`);
-    }
-  
-    throw new Error("Failed to connect to device after multiple attempts");
-  }
 
-  const connectToWebBluetoothDevice = async () => {
-    try {
-      const device = await (navigator as any).bluetooth.requestDevice({
-        filters: [{ namePrefix: 'CPS' }, {services: ["0000feaa-0000-1000-8000-00805f9b34fb", "6e400001-b5a3-f393-e0a9-e50e24dcca9e"]}]
-      });
-      console.log("Connected to device:", device.name);
-      if (device.gatt){
-        console.log(device.gatt)
-      }
-      
-      if (device.gatt.connected) {
-        device.gatt.disconnect();
-        await new Promise(resolve => setTimeout(resolve, 1000));
-      }
-      console.log("o primeiro avião atingiu a torre");
+  const [lastActivities, setLastActivities] = useState<{ activity: string; time: number }[]>([]);
 
-      const server = await connectWithRetry(device);
 
-      console.log("o segundo avião atingiu a torre");
-      
-      const service = await server.getPrimaryService("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
+  // Helper function to generate a random interval between 5 and 30 seconds
+  const getRandomInterval = () => Math.floor(Math.random() * (20 - 5 + 1) + 5) * 1000;
 
-      console.log("eleições presidenciais 2024");
-      
-      const customData = await service.readValue();
-      console.log(customData);
-      setDeviceData(customData); 
-    } catch (error) {
-      console.error("Error connecting to web Bluetooth device:", error);
-    }
-  };
-/*
   useEffect(() => {
-      /*
-      const connectToBluetoothDevice = async () => {
-        // Request permission on Android
-        
-        if (Platform.OS === 'android') {
-          const granted = await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-            {
-              title: 'Location Permission',
-              message: 'Location permission is required for Bluetooth scanning',
-              buttonPositive: 'OK',
-            }
-          );
-          if (granted !== PermissionsAndroid.RESULTS.GRANTED) {
-            console.log("Location permission denied.");
-            return;
-          }
+
+    let interval = getRandomInterval()
+
+    // Set a random interval to change the activity
+    const activityInterval = setInterval(() => {
+
+      // Update the state to add the new activity
+      setLastActivities((prevActivities) => {
+        // Add the new activity to the front of the array
+        const updatedActivities = [{activity: currentActivity, time: interval/1000}, ...prevActivities];
+
+        // Ensure the array doesn't exceed 5 items
+        if (updatedActivities.length > 5) {
+          updatedActivities.pop(); // Remove the last item (oldest activity)
         }
 
-        // Attempt to connect to the device
-        try {
-          const device = await manager.connectToDevice(DEVICE_ADDRESS);
-          console.log("Connected to device:", device.name);
+        return updatedActivities;
+      });
+      setCurrentActivityIndex((prevIndex) => (prevIndex + 1) % activityNames.length);
+    }, interval);
 
-          await device.discoverAllServicesAndCharacteristics();
-          
-          // Replace with the actual service and characteristic UUIDs for your device
-          const targetServiceUUID = "YOUR_SERVICE_UUID";
-          const targetCharacteristicUUID = "YOUR_CHARACTERISTIC_UUID";
-          
-          const characteristic = await device.readCharacteristicForService(targetServiceUUID, targetCharacteristicUUID);
-          const value = characteristic.value;  // Data received from the device
-          setDeviceData(value);
-        } catch (error) {
-          console.log("Error connecting to device:", error);
-        }
-      };
+    // Cleanup interval on unmount or before setting a new interval
+    return () => clearInterval(activityInterval);
+  }, [currentActivityIndex]);
 
-      // Connect as soon as the component mounts
-      connectToBluetoothDevice();
+  useEffect(() => {
 
-      // Clean up the BLE manager on unmount
-      return () => manager.destroy();
     
-  }, []);*/
+    // Reset time elapsed to 0 when activity changes
+    setTimeElapsed(0);
+
+    // Start counting time for current activity
+    const timeInterval = setInterval(() => {
+      setTimeElapsed((prevTime) => prevTime + 1);
+    }, 1000);
+
+    // Cleanup timer when activity changes
+    return () => clearInterval(timeInterval);
+  }, [currentActivityIndex]);
 
   return (
-    <View>
-      <View className="w-screen px-1 py-2 border-b-[3px] border-indigo-600 items-center">
+    <View className='bg-white	'>
+      <View className="mt-6 -screen px-1 py-2 border-b-[3px] border-indigo-600 items-center">
         <Text className="text-xl">What Thing Doing</Text>
       </View>
       
@@ -154,12 +82,16 @@ export default function App() {
           source={currentActivityPNG}
           style={{ width: 200, height: 200 }}
         />
-        <Text>{currentActivity}</Text>
-        <Text>{deviceData ? `Device Data: ${deviceData}` : "No Data from Device"}</Text>
-        {/* Button to connect to Bluetooth device */}
-        {Platform.OS === "web" && (
-          <Button title="Connect to Bluetooth" onPress={connectToWebBluetoothDevice} />
-        )}
+        <Text className='mt-2'>{currentActivity}</Text>
+        <Text>Time Elapsed: {timeElapsed} seconds</Text>
+        <View className='mt-4'>
+          <Text>Recent Activities:</Text>
+          {lastActivities.map((entry, index) => (
+            <View key={index} style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Text>{entry.activity} - {entry.time} seconds</Text>
+            </View>
+          ))}
+        </View>
       </View>
     </View>
   );
